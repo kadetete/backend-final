6b40b9d0b8ba40d48349127eac3cae40f14b4562
Guilherme Cadete <110258437+kadetete@users.noreply.github.com>
2023-07-10 08:57:22 -0300

    Update dados.service.ts

--- frontend/src/app/dados.service.ts
+++ frontend/src/app/dados.service.ts
@@ -11,6 +11,6 @@ export class DadosService {
   constructor(private http: HttpClient) { }
 
   getDados(): Observable<any> {
-    return this.http.get('http://localhost:3000/alunocarro');
+    return this.http.get('http://localhost:3000/lavouras');
   }
 }
