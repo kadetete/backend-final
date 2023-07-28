7d7c47c05d2b2f2d3d99c9e303aa99eb0a28f970
Rafael-ASS <rarafaelalgusto@gmail.com>
2023-07-28 14:32:39 -0300

    coisa 1.0

--- frontend/src/app/lavouras/lavouras.component.ts
+++ frontend/src/app/lavouras/lavouras.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./lavouras.component.css']
 })
 export class LavourasComponent {
+  div_principal: any = document.getElementById(`div_principal`) as HTMLInputElement
+  bodyUpload(element: any) {  
+    var numLavoura = 0
+    for (var i = 0; i < numLavoura; i++) {
+      var  lavoura = ''
+      var div_secundaria = document.createElement('div');
+      div_secundaria.id = 'id_'+lavoura
+      div_secundaria.id.includes(lavoura)
+      div_secundaria.appendChild(div_secundaria)
 
+
+      this.div_principal.appendChild(div_secundaria);
+    }
+  }
 }
